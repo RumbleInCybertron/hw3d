da0c445a58f22e913cbe5e80fb3fc2dbdb0eaab8
Derek <RumbleInCybertron@users.noreply.github.com>
2020-03-15 15:14:54 +0100

    WM_ Logging
    TODO: enter contents of mappings, and setup operator()

--- hw3d/WindowsMessageMap.cpp
+++ hw3d/WindowsMessageMap.cpp
@@ -5,3 +5,21 @@
 #include <iomanip>
 
 // top secret messages
+#define WM_UAHDESTROYWINDOW 0x0090
+#define WM_UAHDRAWMENU 0x0091
+#define WM_UAHDRAWMENUITEM 0x0092
+#define WM_UAHINITMENU 0x0093
+#define WM_UAHMEASUREMENUITEM 0x0094
+#define WM_UAHNCPAINTMENUPOPUP 0x0095
+
+#define REGISTER_MESSAGE(msg){msg,#msg}
+
+WindowsMessageMap::WindowsMessageMap()
+	:
+	map({})
+{}
+
+std::string WindowsMessageMap::operator()(DWORD msg, LPARAM lp, WPARAM wp) const
+{
+	return NULL;
+}
